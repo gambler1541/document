812564da012f9ec8f5f916127de499b820ad908d
pmb <gambler1541@gmail.com>
2018-06-18 15:08:04 +0900

    show_friends()구현

--- app/models/many_to_many/models/self.py
+++ app/models/many_to_many/models/self.py
@@ -8,10 +8,21 @@ __all__ = (
 class FacebookUser(models.Model):
     name = models.CharField(max_length=50)
     # 관계가 대칭적으로 형성됨
-    #   A가 B를 friends에 추가 -> B의 friends에도 A가 추가되어있음
+    #   A가 B를 friends에 추가 -> B의 friends에도 A가 추가되어있
     friends = models.ManyToManyField(
         'self',
     )
 
     def __str__(self):
         return self.name
+
+    def show_friends(self):
+        print(f'{self.name}의 친구목록')
+        for friend in self.friends.all():
+            print(f'- {friend.name}')
+        print(f'총 ({len(self.friends.all())}명)')
+
+
+
+
+
